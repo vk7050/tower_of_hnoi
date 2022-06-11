2b4c3f17f224db8322d3cd7b42a120ca42869260
vk7050 <84660046+vk7050@users.noreply.github.com>
2022-06-11 11:31:42 +0530

    Update tower.cpp

--- tower.cpp
+++ tower.cpp
@@ -1,2 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void TOWER(char s,char d,char e,int n)
+{
+  if(n>0)
+  {
+    TOWER(s,e,d,n-1);
+      cout<<n<<" transfer from "<<s<<" to "<<d<<endl;
+    TOWER(e,d,s,n-1);
+  }
+}
+int main()
+{
+  int n;
+  cin>>n;
+  char s='s',d='d',e='e';
+  TOWER(s,d,e,n);
+  return 0;
+}
